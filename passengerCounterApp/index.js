dcbcbe505dfc119c4b694988aa74a12e4eb19308
J <116361492+j-archives@users.noreply.github.com>
2024-07-04 05:26:50 +0000

    added new function for save button

--- passengerCounterApp/index.js
+++ passengerCounterApp/index.js
@@ -1,12 +1,23 @@
 // document.getElementById("count").innerText = 5
-
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 console.log(countEl)
 let count = 0
 
 function increment() {
     count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = " " + count + " - "
+    saveEl.innerText += countStr
+    console.log(count)
+}
+
+function reset() {
+    count = 0
     countEl.innerText = count
 }
 
