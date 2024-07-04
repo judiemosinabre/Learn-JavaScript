9b4bcdce3fc03045265c0c6464ef4ca4b23ac299
J <116361492+j-archives@users.noreply.github.com>
2024-07-04 00:32:42 +0000

    updated js

---
+++ passengerCounterApp/index.js
@@ -0,0 +1,15 @@
+// document.getElementById("count").innerText = 5
+
+let countEl = document.getElementById("count-el")
+
+// console.log(countEl)
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+
+
+
