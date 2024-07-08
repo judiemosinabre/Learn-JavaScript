da9b972d0c8dc17c881e2409b81b84d0369f92c0
J <116361492+j-archives@users.noreply.github.com>
2024-07-08 03:25:58 +0000

    new file for rps

---
+++ rockPaperScissor/index.js
@@ -0,0 +1,12 @@
+let hands = ["rock", "paper", "scissor"]
+
+
+let randomNumber = Math.floor(Math.random()*3)
+console.log(randomNumber)
+
+function randomItem() {
+    let randItem = hands[randomNumber]
+    return randItem
+}
+
+console.log(randomItem())
