581b5c6db32a8c675ccb686aebedcdf426be8e1d
J <116361492+j-archives@users.noreply.github.com>
2024-07-08 03:35:40 +0000

    commented a line of code

--- rockPaperScissor/index.js
+++ rockPaperScissor/index.js
@@ -2,7 +2,7 @@ let hands = ["rock", "paper", "scissor"]
 
 
 let randomNumber = Math.floor(Math.random()*3)
-console.log(randomNumber)
+// console.log(randomNumber)
 
 function randomItem() {
     let randItem = hands[randomNumber]
