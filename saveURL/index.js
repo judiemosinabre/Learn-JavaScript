f18426cfa726e06783f0533d96389299e788eae1
J <116361492+j-archives@users.noreply.github.com>
2024-07-18 05:28:33 +0000

    implemented local storage

--- saveURL/index.js
+++ saveURL/index.js
@@ -1,12 +1,27 @@
 let myLeads = []
 
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("https://v2.scrimba.com/learn-javascript-c0v/~04j")
+// console.log(typeof myLeads)
+
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
+
 //const = constant, cannot re-assign value
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
     clearText(inputEl)
 
