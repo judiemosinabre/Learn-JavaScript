4cc25dd356f4897c6e83feb4ba007bc123930eae
J <116361492+j-archives@users.noreply.github.com>
2024-07-18 05:40:09 +0000

    added double click function for delete all button

--- saveURL/index.js
+++ saveURL/index.js
@@ -11,12 +11,19 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
