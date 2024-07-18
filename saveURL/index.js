6688b06da6164bdbfcdc19d23e6adeb09bf6d11f
J <116361492+j-archives@users.noreply.github.com>
2024-07-18 05:35:10 +0000

    changed leads to const variable

--- saveURL/index.js
+++ saveURL/index.js
@@ -12,7 +12,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
