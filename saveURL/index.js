0ec2769794998a4d798b0b3e906bfb87b7de2751
J <116361492+j-archives@users.noreply.github.com>
2024-07-18 09:22:21 +0000

    modified tab button function

--- saveURL/index.js
+++ saveURL/index.js
@@ -12,12 +12,30 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+// const tabs = [
+//     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+// ]
+
+tabBtn.addEventListener("click", function() {
+    // console.log(tabs[0].url)
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+})
+
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
