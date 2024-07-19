2e5e6e50fe4984353066781d75b2b5a3956a0a87
J <116361492+j-archives@users.noreply.github.com>
2024-07-19 02:59:31 +0000

    access the dom for converter

--- unitConverter/index.js
+++ unitConverter/index.js
@@ -9,8 +9,16 @@ convertBtn.addEventListener("click", function(){
 function lengthConvert() {
     let toFeet = (inputEl.value*3.281).toFixed(3)
     let toMeters = (inputEl.value/3.281).toFixed(3)
+    let toLiters = (inputEl*0.264).toFixed(3)
+    let toGallons = (inputEl/0.264).toFixed(3)
+    let toMass = (inputEl*2.204).toFixed(3)
+    let toPounds = (inputEl/2.204).toFixed(3)
+
     lengthConverter.innerHTML += 
     `<p>
         ${inputEl.value} meters = ${toFeet} feet | ${inputEl.value} feet = ${toMeters} meters
     </p>`
+
+
+    
 }
