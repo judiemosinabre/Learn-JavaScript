ef1e7ada166745f42467e14ddf9dda725e95b546
J <116361492+j-archives@users.noreply.github.com>
2024-07-19 12:18:28 +0000

    initial commit

--- saveURLMobile/index.js
+++ saveURLMobile/index.js
@@ -1,53 +1,31 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js"
+require("dotenv").config()
 
-// myLeads = JSON.parse(myLeads)
-// myLeads.push("https://v2.scrimba.com/learn-javascript-c0v/~04j")
-// console.log(typeof myLeads)
+const firebaseConfig = {
+    // databaseURL: "https://leads-tracker-app-d1f10-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: process.env.DATABASE_URL
+}
 
-// myLeads = JSON.stringify(myLeads)
-// console.log(typeof myLeads)
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
 
-//const = constant, cannot re-assign value
+console.log(firebaseConfig.databaseURL)
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-// const tabs = [
-//     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-// ]
-
-tabBtn.addEventListener("click", function() {
-    // console.log(tabs[0].url)
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        console.log(tabs)
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads",JSON.stringify(myLeads))
-        render(myLeads)
-
-    })
-})
 
 
 deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    
 })
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    render(myLeads)
+    console.log(inputEl.value)
     clearText(inputEl)
 
 })
@@ -57,21 +35,15 @@ function clearText(inputEl) {
 }
 
 function render(leads) {
-    // placeholder for listItems
     let listItem = ""
 
     for (let i=0; i<leads.length; i++){
-        // listItem += "<li><a target='_blank' href ='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItem += `
         <li>
             <a target='_blank' href ='${leads[i]}'> 
                 ${leads[i]}
             </a>
         </li>`
-
-        // create element --> const li = document.createElement("li")
-        // set text content --> li.textContent = myLeads[i]
-        // append to ul --> ulEl.append(li)
 
     }
 
