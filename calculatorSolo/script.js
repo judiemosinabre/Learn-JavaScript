9ea96a4780185e26053ead491f6b596253fce5c8
J <116361492+j-archives@users.noreply.github.com>
2024-07-22 11:51:58 +0000

    ac, clear, plus, and equal button are now working

--- calculatorSolo/script.js
+++ calculatorSolo/script.js
@@ -13,91 +13,129 @@ const sevenBtn = document.getElementById("seven")
 const eightBtn = document.getElementById("eight")
 const nineBtn = document.getElementById("nine")
 
+const history = document.getElementById("history")
 const allClear = document.getElementById("all-clear")
+const clear = document.getElementById("clear")
 const plus = document.getElementById("plus")
+const equal = document.getElementById("equal")
 
-let numHolder = []
+let firstNum = []
+let secondNum = []
+
+let plusActive = false
+
+let finalAnswer = 0
 
 allClear.addEventListener("click",function() {
-    numHolder = []
-    console.log(numHolder)
+    firstNum = []
+    console.log(firstNum)
     answer.textContent ="‎"
+    history.textContent ="‎"
     answer.style.color = "#222"
     answer.style.border = "1px solid white"
 })
 
-plus.addEventListener("click",function(){
-    
+clear.addEventListener("click", function(){
+    firstNum.pop()
+    answer.textContent = firstNum.join("")
+
 })
+
+plus.addEventListener("click",function(){
+    history.textContent = firstNum.join("")
+    secondNum = firstNum
+    firstNum = []
+    answer.textContent = "‎" //empty
+    plusActive = true
+
+    console.log(secondNum)
+    console.log(firstNum)
+})
+
+equal.addEventListener("click", function(){
+    history.textContent ="‎"
+    if (plusActive) {
+        answer.textContent = sumGetter()
+        console.log(sumGetter())
+    }
+})
+
+
+function sumGetter(){
+    firstNum = parseInt(firstNum.join(''), 10) //10 = decimal
+    secondNum = parseInt(secondNum.join(''), 10)
+
+    return firstNum + secondNum
+}
 
 //number functions
 zeroBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(0)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(0)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 oneBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(1)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(1)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 twoBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(2)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(2)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 threeBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(3)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(3)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 fourBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(4)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(4)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 fiveBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(5)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(5)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 sixBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(6)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(6)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 sevenBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(7)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(7)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 eightBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(8)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(8)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
 nineBtn.addEventListener("click", function(){
-    console.log(numHolder)
-    numHolder.push(9)
-    answer.textContent = numHolder.join("")
+    console.log(firstNum)
+    firstNum.push(9)
+    answer.textContent = firstNum.join("")
     answer.style.color = "white"
 })
 
